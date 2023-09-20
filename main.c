654a9eaf28b778b4aa306b9b60bbc049933d881f
masichaivy <lyndamasicha@gmail.com>
2023-09-20 08:03:13 -0700

    modify main functions

--- main.c
+++ main.c
@@ -1,48 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
+#include <string.h>
+
+stack_t *stack = NULL;
+
+void process_instructions(FILE *file, instruction_t *instructions, int
+			  num_instructions)
+{
+	opcode_data_t op_data;
+	char line[MAX_LINE_LENGTH];
+	unsigned int line_number;
+	char *token;
+
+	line_number = 1;
+	while (fgets(line, sizeof(line), file))
+	{
+		char *trimmed_line = line;
+
+		while (*trimmed_line == ' ' || *trimmed_line == '\t')
+		{
+			trimmed_line++; }
+		if (*trimmed_line == '#' || *trimmed_line == '\n')
+		{
+			line_number++;
+			continue; }
+		token = strtok(line, " \t\n");
+		if (token)
+		{
+			instruction_t *instruction;
+
+			instruction = find_instruction(token, instructions, num_instructions);
+			if (instruction)
+			{
+				token = strtok(NULL, " \t\n");
+				if (token)
+				{
+					op_data.value = token; }
+				else
+				{
+					op_data.value = NULL; }
+				instruction->f(&stack, line_number, &op_data); }
+			else
+			{
+				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
+				exit(EXIT_FAILURE); }
+		}
+		line_number++;
+	}
+}
+
+instruction_t *find_instruction(char *opcode, instruction_t *instructions, int
+				num_instructions)
+{
+	int i;
+
+	for (i = 0; i < num_instructions; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			return (&instructions[i]); }
+	}
+
+	return (NULL);
+}
 
 /**
- * main - Entry point to the monty program
- * @ac: argument count to main
- * @av: an array of arguments to main
+ * main - main function for monty
+ * @argc: lentgh of argv
+ * @argv: array
  *
- * Return: On Success, EXIT_SUCCESS. On failure, EXIT_FAILURE
+ * Return: O or 1
  */
-
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
-	FILE *fd;
-	int lineno = 0/*, i = 0*/;
-	size_t len = 0;
-	ssize_t line_read;
-	char *line = NULL, **words = NULL;
-	stack_t *top = NULL;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"nop", nop},
+		{"swap", swap},
+		{"add", add},
+		{"pint", pint},
+		{"pop", pop},
+		{"sub", sub},
+		{"div", div_ide},
+		{"mul", mul},
+		{"mod", mod}
+	};
 
-	if (ac != 2)
+	FILE *file;
+	int num_instructions;
+
+	num_instructions = sizeof(instructions) / sizeof(instructions[0]);
+
+	if (argc != 2)
 	{
-		dprintf(2, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE); }
 
-	fd = fopen(av[1], "r");
-	if (fd == NULL)
+
+	file = fopen(argv[1], "r");
+	if (!file)
 	{
-		dprintf(2, "Error: Can't open file %s\n", av[1]);
-		exit(EXIT_FAILURE);
-	}
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		free_stack(&stack);
+		return (EXIT_FAILURE); }
 
-	while ((line_read = getline(&line, &len, fd)) != -1)
-	{
-		lineno++;
-		words = parse_line(line);
-		if (words)
-		{
-			if (strcmp(words[0], "push") == 0)
-				push(words[1], &top, lineno);
-			else
-				check_opcode(words[0], lineno, top);
-		}
-	}
+	process_instructions(file, instructions, num_instructions);
 
-	fclose(fd);
-	exit(EXIT_SUCCESS);
+	fclose(file);
+	free_stack(&stack);
+	return (EXIT_SUCCESS);
+
 }
